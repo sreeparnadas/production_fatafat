234515937e42e11f15864960120ff0724d0b29fa
sreeparnadas <sreeparnadas0675@gmail.com>
2021-07-29 00:38:43 +0530

    cpanel report updated

---
+++ 762-es2015.7ea749521cbb622e3165.js
@@ -0,0 +1,1 @@
+(self.webpackChunkaccounts=self.webpackChunkaccounts||[]).push([[762],{92762:function(n,c,t){"use strict";t.d(c,{vV:function(){return s}}),t(19861),t(58378);var u=t(35366),e=(t(31041),t(87064));t(97388);let s=(()=>{class n{}return n.\u0275fac=function(c){return new(c||n)},n.\u0275mod=u.oAB({type:n}),n.\u0275inj=u.cJS({imports:[[e.BQ,e.si],e.BQ]}),n})()}}]);
